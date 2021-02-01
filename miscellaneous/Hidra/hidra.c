734a69a6e24ca8461890325ad481a22481a9d046
Safin Singh <safin.singh@gmail.com>
2021-02-01 10:05:47 -0800

    fix compilation issue

--- miscellaneous/Hidra/hidra.c
+++ miscellaneous/Hidra/hidra.c
@@ -6,13 +6,13 @@ int main()
     char in[20];
     scanf("%s", in);
 
-    uint32_t __null = (~0x65746673) ^ 1;
+    uint32_t _null = (~0x65746673) ^ 1;
     uint32_t __nullptr = (~0x647b6c30) ^ 2;
     uint32_t __nullerptr = (~0x6c5f6730) ^ 3;
     uint32_t __nullestptr = (~0x5f407740) ^ 4;
     uint32_t __pp_ptr = (~0x797d0000) ^ 5;
 
-    uint32_t __gcc_intrinsic[] = {__null, __nullptr, __nullerptr, __nullestptr, __pp_ptr};
+    uint32_t __gcc_intrinsic[] = {_null, __nullptr, __nullerptr, __nullestptr, __pp_ptr};
     for (int i = 0; i < 5; i++)
     {
         uint32_t lolz = __gcc_intrinsic[i] ^ (i + 1);
