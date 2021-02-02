ca403658466baaf10fe1b7347f8cc97d577a6351
Safin Singh <safin.singh@gmail.com>
2021-02-02 11:31:56 -0800

    [fix] make hidra a tiny bit more descriptive

--- miscellaneous/Hidra/hidra.c
+++ miscellaneous/Hidra/hidra.c
@@ -1,9 +1,19 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+char in[20];
+void death(int i)
+{
+    if (i + 1 != sizeof(in) / sizeof(&in))
+    {
+        printf("\nuh yeah not it\n");
+        exit(1);
+    }
+}
 
 int main()
 {
-    char in[20];
     scanf("%s", in);
 
     uint32_t _null = (~0x65746673) ^ 1;
@@ -11,8 +21,8 @@ int main()
     uint32_t __nullerptr = (~0x6c5f6730) ^ 3;
     uint32_t __nullestptr = (~0x5f407740) ^ 4;
     uint32_t __pp_ptr = (~0x797d0000) ^ 5;
-
     uint32_t __gcc_intrinsic[] = {_null, __nullptr, __nullerptr, __nullestptr, __pp_ptr};
+
     for (int i = 0; i < 5; i++)
     {
         uint32_t lolz = __gcc_intrinsic[i] ^ (i + 1);
@@ -20,8 +30,12 @@ int main()
         for (int b = 0; b < 4; b++)
         {
             uint8_t byte = ((temp >> (8 * 3 - (b * 8))) & 0xFF);
-            if (in[i * 4 + b] == byte)
+            if ((in[i * 4 + b] == byte))
                 printf("%c", byte);
+            else
+                death(i * 4 + b);
         }
     }
+
+    printf("\nyep!\n");
 }
